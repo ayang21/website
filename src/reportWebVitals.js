8b131eec5a96a2055e8feac8546e293f54416de0
Adam Yang <Adamyang2002@gmail.com>
2024-10-29 19:04:16 -0400

    Update import call

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,12 @@
+import { onCLS, onFCP, onLCP, onTTFB } from 'web-vitals';
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+      onCLS(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    };
+  };
 
 export default reportWebVitals;
