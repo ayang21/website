58f32f91d6925f2709aa84253f20aafd4120110a
Adam Yang <Adamyang2002@gmail.com>
2024-10-29 18:02:53 -0400

    Update import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
-import Nav from './Nav';
-import Footer from './Footer';
-import Home from './Home';
+import Nav from './Pages/Nav';
+import Footer from './Pages/Footer';
+import Home from './Pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -27,7 +27,18 @@ setTimeout(() => {
   }, {once: true});
   const ring = document.querySelector('.ring');
   ring.innerHTML = 'Ready';
-  // ring.parentNode.removeChild(ring);
+  ring.style.cursor = 'pointer';
+
+  ring.addEventListener('click', () => {
+    root.render(
+      <React.StrictMode>
+        <Nav />
+        <Home />
+        <Footer />
+      </React.StrictMode>
+    );
+  });
+
 }, 2000);
 
 reportWebVitals(console.log);
