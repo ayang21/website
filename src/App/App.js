6e6326673d97f74275bbb0cb94e1ece5a0cde56b
Adam Yang <Adamyang2002@gmail.com>
2024-10-29 18:03:26 -0400

    Update imports and move to relevant folders

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import logoTop from '../imgs/logo-landing-top.png';
+import logoBot from '../imgs/logo-landing-bottom.png';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logoTop} className="App-logo" alt="logo-top" />
+        <img src={logoBot} className="App-logo-bottom" alt="logo-bottom" /> 
+        <div class="ring">Loading
+          <span></span>
+        </div>
+        <p>
+          Press <button type="button" className='press-esc' >esc</button> on your keyboard to exit into the home page. <p style={{fontSize: "10px"}}>If it is not working, you might want to refresh the page <br/> Or you can press "Ready" to continue</p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
