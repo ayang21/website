2b8fe83b2a16a96a1105bbc26877c901ca134e22
Adam Yang <Adamyang2002@gmail.com>
2024-11-20 13:16:25 -0500

    Chore: Set styling for footer message

--- src/App/App.js
+++ src/App/App.js
@@ -14,8 +14,9 @@ function App() {
           <span></span>
         </div>
         <p>
-          Press <button type="button" className='press-esc' >esc</button> on your keyboard to exit into the home page. <span style={{fontSize: "10px"}}>If it is not working, you might want to refresh the page <br/> Or you can press "Ready" to continue</span>
+          Press <button type="button" className='press-esc' >esc</button> on your keyboard to exit into the home page. 
         </p>
+        <span style={{fontSize: "10px"}}>If it is not working, you might want to refresh the page <br/> Or you can press "Ready" to continue</span>
       </header>
     </div>
   );
