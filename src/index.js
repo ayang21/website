671f768abd87229742da923fe7a96ceca15fa1c2
Adam Yang <Adamyang2002@gmail.com>
2024-11-25 16:36:46 -0500

    Chore: UPdate index rendering

--- src/index.js
+++ src/index.js
@@ -2,43 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
-import reportWebVitals from './reportWebVitals';
 import Nav from './Pages/Nav/Nav';
 import Footer from './Pages/Footer/Footer';
 import Home from './Pages/Home/Home';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function landing(){
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+function redirect() {
+  root.render(
+    <React.StrictMode>
+      <Nav />
+      <Home />
+      <Footer />
+    </React.StrictMode>
+  );
+}
+
+landing();
 setTimeout(() => {
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') { 
-      root.render(
-        <React.StrictMode>
-          <Nav />
-          <Home />
-          <Footer />
-        </React.StrictMode>
-      );
+      redirect();
     }
   }, {once: true});
   const ring = document.querySelector('.ring');
   ring.innerHTML = 'Ready';
   ring.style.cursor = 'pointer';
-
+  
   ring.addEventListener('click', () => {
-    root.render(
-      <React.StrictMode>
-        <Nav />
-        <Home />
-        <Footer />
-      </React.StrictMode>
-    );
+    redirect();
   });
-
 }, 2000);
 
 reportWebVitals(console.log);
